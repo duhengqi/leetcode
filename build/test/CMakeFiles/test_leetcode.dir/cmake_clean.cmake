e2ca505633fb389d4b858d4b62d464a74edf23d5
duhengqi <1292071940@qq.com>
2024-07-08 08:44:14 +0800

    sync leetcode

---
+++ build/test/CMakeFiles/test_leetcode.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_leetcode.dir/test_learnalgorithm.cpp.obj"
+  "CMakeFiles/test_leetcode.dir/test_learnalgorithm.cpp.obj.d"
+  "D:/code/leetcode/bin/libtest_leetcode.dll.a"
+  "D:/code/leetcode/bin/test_leetcode.exe"
+  "D:/code/leetcode/bin/test_leetcode.exe.manifest"
+  "D:/code/leetcode/bin/test_leetcode.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_leetcode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
