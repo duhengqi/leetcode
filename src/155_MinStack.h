63ee63024423e14d9eeb3862d9334386edf411d8
Hengqi Du <Hengqi.du@belden.com>
2024-07-08 09:34:12 +0800

    增加155 232 problems

---
+++ src/155_MinStack.h
@@ -0,0 +1,58 @@
+#include "god.h"
+/*
+ * @lc app=leetcode.cn id=155 lang=cpp
+ *
+ * [155] 最小栈
+ */
+
+// @lc code=start
+#define MAX_STK_SIZE  (3*10*10*10*10)
+
+class MinStack_155 {
+public:
+    MinStack_155() {}
+    
+    void push(int val) {
+        if (stkHead == MAX_STK_SIZE) {
+            return;
+        }
+        stkHead++;
+        stk[stkHead] = val;
+    }
+    
+    void pop() {
+        stkHead--;
+    }
+    
+    int top() {
+        return stk[stkHead];
+    }
+    
+    int getMin() {
+        if (stkHead < 0) {
+            return 0;
+        }
+        int res = stk[0];
+        for (int i = 0; i <= stkHead; i++) {
+            if (res > stk[i]) {
+                res = stk[i];
+            }
+        }
+        return res;
+    }
+private:
+    int stk[MAX_STK_SIZE] = {0};
+    int stkHead = -1;
+
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+// @lc code=end
+
---
+++ src/232_MyQueue.h
@@ -0,0 +1,67 @@
+#include "god.h"
+/*
+ * @lc app=leetcode.cn id=232 lang=cpp
+ *
+ * [232] 用栈实现队列
+ */
+
+// @lc code=start
+class MyQueue_232 {
+public:
+    MyQueue_232() {
+
+    }
+
+    void push(int x) { lStk.push(x); }
+
+    int pop() {
+        if (lStk.empty()) {
+            return -1;
+        }
+        while (!lStk.empty()) {
+            rStk.push(lStk.top());
+            lStk.pop();
+        }
+        int res = rStk.top();
+        rStk.pop();
+        while(!rStk.empty()) {
+            lStk.push(rStk.top());
+            rStk.pop();  
+        }
+        return res;
+    }
+    
+    int peek() {
+        if (lStk.empty()) {
+            return -1;
+        }
+        while (!lStk.empty()) {
+            rStk.push(lStk.top());
+            lStk.pop();
+        }
+        int res = rStk.top();
+        while(!rStk.empty()) {
+            lStk.push(rStk.top());
+            rStk.pop();  
+        }
+        return res;
+    }
+
+    bool empty() { return lStk.empty(); }
+
+private:
+    stack<int> lStk;
+    stack<int> rStk;
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
+// @lc code=end
+
+
