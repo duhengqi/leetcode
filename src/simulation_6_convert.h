be9dd5282935ba4898a9a01348662c374db343d4
duhengqi <1292071940@qq.com>
2024-07-11 23:40:52 +0800

    合入 模拟类型 number 6 题解

--- src/simulation_6_convert.h
+++ src/simulation_6_convert.h
@@ -9,19 +9,48 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-
+        int row = 0;
+        int col = 0;
+        int index = 0;
+        string result;
+        vector<vector<int>> z(numRows,vector<int>(1000,0));
+        while(index < s.size()){
+            row = 0;
+            while(index < s.size() && row < numRows) {
+                z[row][col] = s[index];
+                index++;
+                row++;
+            }
+            row = numRows - 2;
+            col++;
+            while (index < s.size() && row >0 ) {
+                z[row][col] = s[index];
+                row--;
+                index++;
+                col++;
+            }
+            col++;
+        }
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < col; j++) {
+                if (z[i][j] != 0) {
+                    result += (z[i][j]);
+                }
+            }
+        }
+        return result;
     }
 };
 // @lc code=end
 
-TEST(test_problem_946, testcase1)
+TEST(test_problem_6, testcase1)
 {
     Solution so;
     string result = so.convert("PAYPALISHIRING",3);
     EXPECT_EQ(result,"PAHNAPLSIIGYIR");
 }
 
-TEST(test_problem_946, testcase1)
+TEST(test_problem_6, testcase2)
 {
     Solution so;
     string result = so.convert("PAYPALISHIRING",4);
